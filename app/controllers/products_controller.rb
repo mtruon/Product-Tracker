ec4b560f5629363d9269772752f36f68304550c7
Michael Truong <mtruon02@mail.uoguelph.ca>
2020-01-07 14:43:28 -0500

    Added some functionality for the index product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,35 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find_by_id(params[:id])
+    if @product.nil?
+      # TODO: If no products are found simply display
+      # An option to create a new product
+      flash[:error] = "No products found"
+      render 'new'
+    end
   end
 
   def new
+    @product = Product.new
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def create
+    @product = Product.new(product_params)
+
+    # Validation
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -20,4 +38,8 @@ class ProductsController < ApplicationController
   def destroy
   end
 
+  private
+    def product_params
+      params.require(:product).permit(:name, :url, :domain)
+    end
 end
